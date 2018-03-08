9d6f52ebc5897e5a15135d262177cfad37bab875
Nicolas LOYAU <nicolas.loyau@icloud.com>
2018-03-08 22:55:26 +0100

    simple add route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+  console.log('External call')
+})
+
+app.get('/tutu/', function (req, res) {
+  res.send('Hello tutu!')
 })
 
 app.listen(8080, function () {
